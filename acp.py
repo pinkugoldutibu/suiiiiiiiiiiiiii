d2a1132e330934cd6047eb9b9d01cf53ab039454
pinkugoldutibu <sharolarkansas@gmail.com>
2026-01-10 19:08:57 -0600

    Add files via upload

---
+++ acp.py
@@ -0,0 +1,16 @@
+user = input("do you wanna do a quiz?")
+if user == 'yes':
+    print ("alright")
+    p = input("what is the hardest language in the world?")
+    if p == 'mandarian':
+        print("right")
+        pp = input("in which conteint is france in?")
+        if pp == 'europe':
+            print("you are right")
+            ppp = input("in which contient saudi ariabia in?")
+            if ppp == 'asia':
+                print("alright you win")
+else:
+    print("all right i like your choice")
+
+
